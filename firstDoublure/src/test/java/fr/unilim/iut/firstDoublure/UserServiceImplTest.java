091eccacedeeb837c045ea7aa2a857a15f81a2cb
ThomasCampredon <thomas.campredon@etu.unilim.fr>
2019-11-04 17:19:41 +0100

    fin exo

--- firstDoublure/src/test/java/fr/unilim/iut/firstDoublure/UserServiceImplTest.java
+++ firstDoublure/src/test/java/fr/unilim/iut/firstDoublure/UserServiceImplTest.java
@@ -1,16 +1,21 @@
 package fr.unilim.iut.firstDoublure;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
 import org.junit.Test;
+
+
 
 public class UserServiceImplTest {
 	@Test
 	public void should_create_user_with_hashed_password() {
-    
-    		UserService userService = new UserServiceImpl(???);
-     	
-    		User user = userService.createUser("Bob", "secret");
+		HashProvider hasha = mock(HashProvider.class);
+		UserService userService = new UserServiceImpl(hasha);
+		when(hasha.hash("secret")).thenReturn("secret2");
+ 	
+		User user = userService.createUser("Bob", "secret");
 
-     		assertEquals(user.firstName(), "Bob"); 
-     		assertEquals(user.hashedPassword(), "???"); 
+ 		assertEquals(user.firstName(), "Bob");
+ 		assertEquals(user.hashedPassword(), "secret2");
 	}
 }
